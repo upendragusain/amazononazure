168a6f2fd018b9d57ff890f6027fda61a6b6f550
Upendra Gusain <upendra.gusain@gmail.com>
2020-06-22 16:48:17 +0100

    add mvc view component for cart sumary

---
+++ src/amazononazure/PluralsightMVC/Components/ShoppingCartSummary.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using oinkmvc.Infrastructure;
+using oinkmvc.ViewModels;
+
+namespace oinkmvc.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public ShoppingCartSummary(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var cart = _shoppingCartService.GetCart();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = cart,
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
